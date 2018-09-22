6cb2dec09a3b50c97748c14460274cc2ac2e63d6
thomas <372084406@qq.com>
2018-09-22 22:12:32 +0800

    modified files

--- kinova_control/src/move_test.py
+++ kinova_control/src/move_test.py
@@ -1,68 +1,86 @@
 #! /usr/bin/env python
 
-import rospy
-from trajectory_msgs.msg import JointTrajectory
-from trajectory_msgs.msg import JointTrajectoryPoint
-import argparse
+import rospy, sys
+import moveit_commander
+from moveit_commander import MoveGroupCommander, PlanningSceneInterface
+from moveit_msgs.msg import PlanningScene, ObjectColor, Grasp, PlaceLocation, GripperTranslation, MoveItErrorCodes, Constraints, OrientationConstraint
+from geometry_msgs.msg import PoseStamped, Pose, Quaternion
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+from copy import deepcopy
 
-PI = 3.1415926
-joint_limits = {'upper':(2*PI, 313/180*PI, 2*PI, 330/180*PI, 2*PI, 295/180*PI, 2*PI),
-                'lower':(-2*PI, 47/180*PI, -2*PI, 30/180*PI, -2*PI, 65/180*PI, -2*PI)}
-trajectory_points = [[-1.5,2.9,0.0,1.3,4.2,1.4,0.0],
-                     [0.0,2.9,0.0,1.3,4.2,1.4,0.0],
-                     [1.5,2.9,0.0,1.3,4.2,1.4,0.0],
-                     [0.0,2.9,0.0,1.3,4.2,1.4,0.0]]
+REFERENCE_FRAME  = 'world'
 
-def move_test(command):
-    topic_name = '/j2s7s300/effort_joint_trajectory_controller/command'
-    pub = rospy.Publisher(topic_name, JointTrajectory, queue_size=1)
-    jointcmd = JointTrajectory()
-    jointcmd.header.stamp = rospy.Time.now() + rospy.Duration.from_sec(0.0)
-    jointcmd.joint_names.extend(['j2s7s300_joint_' + str(i+1) for i in range(0,7)])
-    points = []
-    for point_num in range(len(trajectory_points)):
-        points.append(JointTrajectoryPoint())
-        points[point_num].time_from_start = rospy.Duration.from_sec((point_num+1)*5.0)
-        points[point_num].positions.extend(command[point_num][:])
-        points[point_num].velocities.extend([0]*7)
-        points[point_num].accelerations.extend([0]*7)
-        points[point_num].effort.extend([0]*7)
-    jointcmd.points.extend(points)
-    rate = rospy.Rate(100)
-    count = 0
-    while (count < 30):
-        pub.publish(jointcmd)
-        count += 1
-        rate.sleep()
+class Moveit_test():
+    def __init__(self):
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node('moveit_test')
+
+        arm = MoveGroupCommander('arm')
+        end_effector_link = arm.get_end_effector_link()
+        arm.allow_replanning(True)
+        arm.set_planning_time(5)
+
+        target_pose = PoseStamped()
+        target_pose.header.frame_id = REFERENCE_FRAME
+        target_init_orientation = Quaternion()
+        target_init_orientation = quaternion_from_euler(0.0, 1.57, 0.0)
+        self.setPose(target_pose, [0.5, 0.2, 0.5],list(target_init_orientation))
+
+        current_pose = arm.get_current_pose(end_effector_link)
+        self.setPose(current_pose, [current_pose.pose.position.x, current_pose.pose.position.y, current_pose.pose.position.z], list(target_init_orientation))
+        arm.set_pose_target(current_pose)
+        arm.go()
+        rospy.sleep(2)
+
+
+        constraints = Constraints()
+        orientation_constraint = OrientationConstraint()
+        constraints.name = 'gripper constraint'
+        orientation_constraint.header = target_pose.header
+        orientation_constraint.link_name = end_effector_link
+        orientation_constraint.orientation.x = target_init_orientation[0]
+        orientation_constraint.orientation.y = target_init_orientation[1]
+        orientation_constraint.orientation.z = target_init_orientation[2]
+        orientation_constraint.orientation.w = target_init_orientation[3]
+
+        orientation_constraint.absolute_x_axis_tolerance = 0.03
+        orientation_constraint.absolute_y_axis_tolerance = 0.03
+        orientation_constraint.absolute_z_axis_tolerance = 3.14
+        orientation_constraint.weight = 1.0
+
+        constraints.orientation_constraints.append(orientation_constraint)
+        arm.set_path_constraints(constraints)
+
+        arm.set_pose_target(target_pose, end_effector_link)
+        arm.go()
+        rospy.sleep(1)
+
+        moveit_commander.roscpp_shutdown()
+        moveit_commander.os._exit(0)
+        
+
+    def setPose(self, pose_stamped_object, position, orientation=None):
+        if type(pose_stamped_object) is not PoseStamped:
+            raise Exception('Parameter pose_stamped_object must be a PoseStamped object')
+        pose_stamped_object.pose.position.x = position[0]
+        pose_stamped_object.pose.position.y = position[1]
+        pose_stamped_object.pose.position.z = position[2]
+
+        if orientation is None:
+            pose_stamped_object.pose.orientation.w = 1.0
+        else:
+            pose_stamped_object.pose.orientation.x = orientation[0]
+            pose_stamped_object.pose.orientation.y = orientation[1]
+            pose_stamped_object.pose.orientation.z = orientation[2]
+            pose_stamped_object.pose.orientation.w = orientation[3] 
+
 
 if __name__ == '__main__':
-    rospy.init_node('test_msg')
-    rospy.sleep(1)
-    '''
-    home = [0.0,2.9,0.0,1.3,4.2,1.4,0.0]
-    cmd = home
-    for joint_num in range(7):
-        cmd[joint_num] += -1.5
-        if joint_limits['lower'][joint_num] > cmd[joint_num]:
-            cmd[joint_num] = joint_limits['lower'][joint_num]
-        move_test(cmd)
-        rospy.sleep(10)
-        rospy.loginfo('goal 1 finished')
-        cmd = home
-        move_test(cmd)
-        rospy.sleep(10)
-        rospy.loginfo('home goal finished')
-        cmd[joint_num] += 1.5
-        if joint_limits['upper'][joint_num] < cmd[joint_num]:
-            cmd[joint_num] = joint_limits['upper'][joint_num]
-        move_test(cmd)
-        rospy.sleep(10)
-        rospy.loginfo('goal 2 finished')
-        cmd = home
-        move_test(cmd)
-        rospy.sleep(10)
-        rospy.loginfo('home goal finished')
-    '''
-    move_test(trajectory_points)
+    try:
+        Moveit_test()
+    except KeyboardInterrupt:
+        raise
+
     
 
--- kinova_control/src/pick_and_place.py
+++ kinova_control/src/pick_and_place.py
@@ -3,7 +3,7 @@
 import rospy, sys
 import moveit_commander
 from moveit_commander import MoveGroupCommander, PlanningSceneInterface
-from moveit_msgs.msg import PlanningScene, ObjectColor, Grasp, PlaceLocation, GripperTranslation, MoveItErrorCodes
+from moveit_msgs.msg import PlanningScene, ObjectColor, Grasp, PlaceLocation, GripperTranslation, MoveItErrorCodes, Constraints, OrientationConstraint
 from geometry_msgs.msg import PoseStamped, Pose, Quaternion
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
@@ -18,7 +18,7 @@ GRIPPER_NEUTRAL = [0.7, 0.7, 0.7]
 GRIPPER_JOINT_NAMES = ['j2s7s300_joint_finger_1', 'j2s7s300_joint_finger_2', 'j2s7s300_joint_finger_3']
 GRIPPER_EFFORT = [1.0, 1.0, 1.0]
 GRIPPER_FRAME = 'j2s7s300_end_effector'
-REFERENCE_FRAME  = 'root'
+REFERENCE_FRAME  = 'world'
 
 class MoveitObstacle():
     def __init__(self):
@@ -40,57 +40,99 @@ class MoveitObstacle():
         arm.set_planning_time(5)
 
         table_id = 'table'
+        side_id = 'side'
+        table2_id = 'table2'
         box1_id = 'box1'
         box2_id = 'box2'
         target_id = 'target'
+        wall_id = 'wall'
+        ground_id = 'ground'
         scene.remove_world_object(box1_id)
         scene.remove_world_object(box2_id)
         scene.remove_world_object(table_id)
+        scene.remove_world_object(table2_id)
         scene.remove_world_object(target_id)
+        scene.remove_world_object(wall_id)
+        scene.remove_world_object(ground_id)
+        scene.remove_world_object(side_id)
         rospy.sleep(1)
 
         table_ground = 0.55
         table_size = [0.2, 0.7, 0.01]
+        table2_size = [0.25, 0.6, 0.01]
         box1_size = [0.02, 0.9, 0.6]
         box2_size = [0.05, 0.05, 0.15]
         target_size = [0.03, 0.06, 0.10]
+        wall_size = [0.01, 0.9, 0.6]
+        ground_size = [3, 3, 0.01]
+        side_size = [0.01, 0.7, table_ground]
 
         table_pose = PoseStamped()
         table_pose.header.frame_id = REFERENCE_FRAME
         self.setPose(table_pose, [0.7, 0.0, table_ground + table_size[2]/2.0])    
         scene.add_box(table_id, table_pose, table_size)
 
+        side_pose = PoseStamped()
+        side_pose.header.frame_id = REFERENCE_FRAME
+        self.setPose(side_pose, [0.605, 0.0, side_size[2]/2.0])
+        scene.add_box(side_id, side_pose, side_size)
+
+        table2_pose = PoseStamped()
+        table2_pose.header.frame_id = REFERENCE_FRAME
+        self.setPose(table2_pose, [-0.325, 0.0, box1_size[2] + table2_size[2]/2.0])
+    #    scene.add_box(table2_id, table2_pose, table2_size)
+
         box1_pose = PoseStamped()
         box1_pose.header.frame_id = REFERENCE_FRAME
         self.setPose(box1_pose, [-0.36, 0.0, box1_size[2]/2.0]) 
-        scene.add_box(box1_id, box1_pose, box1_size)
+    #    scene.add_box(box1_id, box1_pose, box1_size)
 
         box2_pose = PoseStamped()
         box2_pose.header.frame_id = REFERENCE_FRAME
         self.setPose(box2_pose, [0.63, -0.12, table_ground + table_size[2] + box2_size[2]/2.0])
-        scene.add_box(box2_id, box2_pose, box2_size)
+    #    scene.add_box(box2_id, box2_pose, box2_size)
 
         target_pose = PoseStamped()
         target_pose.header.frame_id = REFERENCE_FRAME
         self.setPose(target_pose, [0.62, 0.0, table_ground + table_size[2] + target_size[2]/2.0])
         scene.add_box(target_id, target_pose, target_size)
+
+        wall_pose = PoseStamped()
+        wall_pose.header.frame_id = REFERENCE_FRAME
+        self.setPose(wall_pose, [-0.405, 0.0, box1_size[2] + table2_size[2] + wall_size[2]/2.0])
+    #    scene.add_box(wall_id, wall_pose, wall_size)
         
-        self.setColor(table_id, 0.8, 0.0, 1.0)
-        self.setColor(box1_id, 0.9, 0.9, 0.9)
-        self.setColor(box2_id, 0.8, 0.4, 1.0)
+        ground_pose = PoseStamped()
+        ground_pose.header.frame_id = REFERENCE_FRAME
+        self.setPose(ground_pose, [0.0, 0.0, -ground_size[2]/2.0])
+    #    scene.add_box(ground_id, ground_pose, ground_size)
+
+        self.setColor(table_id, 0.8, 0.0, 0.0, 1.0)
+    #    self.setColor(table2_id, 0.8, 0.0, 0.0)
+    #    self.setColor(box1_id, 0.9, 0.9, 0.9)
+    #    self.setColor(box2_id, 0.8, 0.4, 1.0)
         self.setColor(target_id, 0.5, 0.4, 1.0)
+    #    self.setColor(wall_id, 0.9, 0.9, 0.9)
+        self.setColor(ground_id, 0.3, 0.3, 0.3, 1.0)
+        self.setColor(side_id, 0.8, 0.0, 0.0, 1.0)
         self.sendColors()
         
+        constraints = Constraints()
+        constraints.name = 'gripper constraint'
+        orientation_constraint = OrientationConstraint()
+
         arm.set_support_surface_name(table_id)
+        
+        test_pose = PoseStamped()
+        test_pose.header.frame_id = REFERENCE_FRAME
+        test_orientation = Quaternion()
+        test_orientation = quaternion_from_euler(0.0, 1.57, -3.14)
+        self.setPose(test_pose, [-0.15, -0.22, box1_size[2] + table2_size[2] + target_size[2]/2.0], list(test_orientation))
 
         place_pose = PoseStamped()
         place_pose.header.frame_id = REFERENCE_FRAME
-        self.setPose(place_pose, [0.64, -0.25, table_ground + table_size[2] + target_size[2]/2.0])
-
-        arm.set_named_target('Vertical')
-        arm.go()
-        rospy.sleep(2)
-
+        self.setPose(place_pose, [0.63, -0.3, table_ground + table_size[2] + target_size[2]/2.0])
+        
         grasp_pose = target_pose
         grasp_init_orientation = Quaternion()
         grasp_init_orientation = quaternion_from_euler(0.0, 1.57, 0.0)        
@@ -100,13 +142,21 @@ class MoveitObstacle():
         grasp_pose.pose.orientation.w = grasp_init_orientation[3]
         grasp_pose.pose.position.x -= 0.02
         rospy.loginfo('quaterion: '+ str(grasp_pose.pose.orientation))
+        
+        orientation_constraint.header = place_pose.header
+        orientation_constraint.link_name = end_effector_link
+        orientation_constraint.orientation.x = grasp_init_orientation[0]
+        orientation_constraint.orientation.y = grasp_init_orientation[1]
+        orientation_constraint.orientation.z = grasp_init_orientation[2]
+        orientation_constraint.orientation.w = grasp_init_orientation[3]
+        orientation_constraint.absolute_x_axis_tolerance = 0.05
+        orientation_constraint.absolute_y_axis_tolerance = 0.05
+        orientation_constraint.absolute_z_axis_tolerance = 3.14
+        orientation_constraint.weight = 1.0
+        constraints.orientation_constraints.append(orientation_constraint)
+        
+        grasps = self.make_grasps(grasp_pose, [table_id], [0.05, 0.07, [1.0, 0.0, 0.0]], [0.22, 0.26, [-1.0, 0.0, 0.0]])
     
-        grasps = self.make_grasps(grasp_pose, [table_id])
-
-        for grasp in grasps:
-            self.gripper_pose_pub.publish(grasp.grasp_pose)
-            rospy.sleep(0.2)
-
         result = None
         n_attempts = 0
         while result != MoveItErrorCodes.SUCCESS and n_attempts < max_pick_attempts:
@@ -114,11 +164,13 @@ class MoveitObstacle():
             n_attempts += 1
             rospy.loginfo('Pick attempt:' + str(n_attempts))
             rospy.sleep(0.2)
+        
+    #    arm.set_path_constraints(constraints)
 
         if result == MoveItErrorCodes.SUCCESS:
             result = None
             n_attempts = 0
-            places = self.make_places(place_pose, [table_id])
+            places = self.make_places(place_pose, [table_id], [0.15, 0.2, [1.0, 0.0, 0.0]], [0.1, 0.12, [-1.0, 0.0, 0.0]])
 
         while result != MoveItErrorCodes.SUCCESS and n_attempts < max_place_attempts:
             for place in places:
@@ -128,12 +180,50 @@ class MoveitObstacle():
 
             n_attempts += 1
             rospy.loginfo('Place attempt:' + str(n_attempts))
-            rospy.sleep(0.2)                    
+            rospy.sleep(0.2)
+        
+        '''
+        grasp_pose = place_pose
+        grasp_pose.pose.orientation.x = grasp_init_orientation[0]
+        grasp_pose.pose.orientation.y = grasp_init_orientation[1]
+        grasp_pose.pose.orientation.z = grasp_init_orientation[2]
+        grasp_pose.pose.orientation.w = grasp_init_orientation[3]
+        grasp_pose.pose.position.x -= 0.02
 
+        grasps = self.make_grasps(grasp_pose, [table_id], [0.05, 0.07, [1.0, 0.0, 0.0]], [0.15, 0.20, [-1.0, 0.0, 0.0]])
+        result = None
+        n_attempts = 0
+        while result != MoveItErrorCodes.SUCCESS and n_attempts < max_pick_attempts:
+            result = arm.pick(target_id, grasps)
+            n_attempts += 1
+            rospy.loginfo('Pick attempt:' + str(n_attempts))
+            rospy.sleep(0.2)
+        '''
+        
+        '''
+        arm.set_support_surface_name(table2_id)
+        
+        self.setPose(place_pose, [-0.26, -0.22, box1_size[2] + table2_size[2] + target_size[2]/2.0])
+        
+        if result == MoveItErrorCodes.SUCCESS:
+            result = None
+            n_attempts = 0
+            places = self.make_places(place_pose, [table2_id], [0.03, 0.06, [-1.0, 0.0, 0.0]], [0.1, 0.15, [1.0, 0.0, 0.0]])
+        
+        while result != MoveItErrorCodes.SUCCESS and n_attempts < max_place_attempts:
+            for place in places:
+                result = arm.place(target_id, place)
+                if result == MoveItErrorCodes.SUCCESS:
+                    break
+
+            n_attempts += 1
+            rospy.loginfo('Place attempt:' + str(n_attempts))
+            rospy.sleep(0.2)
+        '''
         rospy.sleep(2)
         moveit_commander.roscpp_shutdown()
         moveit_commander.os._exit(0)
-
+        
     def setPose(self, pose_stamped_object, position, orientation=None):
         if type(pose_stamped_object) is not PoseStamped:
             raise Exception('Parameter pose_stamped_object must be a PoseStamped object')
@@ -167,12 +257,12 @@ class MoveitObstacle():
 
         self.scene_pub.publish(p)
 
-    def make_grasps(self, initial_pose_stamped, allowed_touch_objects):
+    def make_grasps(self, initial_pose_stamped, allowed_touch_objects, pre, post):
         g = Grasp()
         g.pre_grasp_posture = self.make_gripper_posture(GRIPPER_OPEN)
         g.grasp_posture = self.make_gripper_posture(GRIPPER_CLOSED)
-        g.pre_grasp_approach = self.make_gripper_translation(0.05, 0.07, [1.0, 0.0, 0.0])
-        g.post_grasp_retreat = self.make_gripper_translation(0.22, 0.26, [-1.0, 0.0, 0.0])
+        g.pre_grasp_approach = self.make_gripper_translation(pre[0], pre[1], pre[2])
+        g.post_grasp_retreat = self.make_gripper_translation(post[0], post[1], post[2])
 
         g.grasp_pose = initial_pose_stamped
         roll_vals = [0, 0.1, -0.1, 0.15, -0.15, 0.25, -0.25]
@@ -225,18 +315,16 @@ class MoveitObstacle():
 
         return g
 
-    def make_places(self, init_pose, allowed_touch_objects):
+    def make_places(self, init_pose, allowed_touch_objects, pre, post, roll_vals=[0], pitch_vals=[0], yaw_vals=[0]):
         place = PlaceLocation()
         place.post_place_posture = self.make_gripper_posture(GRIPPER_OPEN)
-        place.pre_place_approach = self.make_gripper_translation(0.2, 0.25, [1.0, 0.0, 0.0])
-        place.post_place_retreat = self.make_gripper_translation(0.1, 0.12, [-1.0, 0.0, 0.0])
+        place.pre_place_approach = self.make_gripper_translation(pre[0], pre[1], pre[2])
+        place.post_place_retreat = self.make_gripper_translation(post[0], post[1], post[2])
 
         place.place_pose = init_pose 
         x_vals = [0, 0.005, 0.01, 0.015, -0.005, -0.01, -0.015]
         y_vals = [0, 0.005, 0.01, 0.015, -0.005, -0.01, -0.015]
 
-        pitch_vals = [0]
-        yaw_vals = [0]
         places = []
 
         for yaw in yaw_vals:
